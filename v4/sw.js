84081c02ae1621490c0856e5a465e878152f047a
dttg123 <dttg123@gmail.com>
2026-09-14 01:46:35 +0900

    Fix DividendOS V4 mobile layout: sw.js

--- v4/sw.js
+++ v4/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dividend-os-v0.9.4-r19';
+const CACHE = 'dividend-os-v0.9.4-r20';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './app.js', './firebase.js', './auth.js', './storage.js', './cloud.js', './backup.js', './runtime-config.js', './toss-client.js',
