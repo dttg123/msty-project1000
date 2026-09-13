86135430f501ede0926fe0d935e88c02cb5b5cc8
dttg123 <dttg123@gmail.com>
2026-09-14 02:17:09 +0900

    Redesign DividendOS V4 home: sw.js

--- v4/sw.js
+++ v4/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dividend-os-v0.9.4-r20';
+const CACHE = 'dividend-os-v0.9.4-r21';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './app.js', './firebase.js', './auth.js', './storage.js', './cloud.js', './backup.js', './runtime-config.js', './toss-client.js',
