0960bafc60d7837343d002dfd9051d6017906c0f
dttg123 <dttg123@gmail.com>
2026-09-13 21:53:04 +0900

    V4 r14 future-date fix: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dividend-os-v0.9.1-r13';
+const CACHE = 'dividend-os-v0.9.1-r14';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './app.js', './firebase.js', './auth.js', './storage.js', './cloud.js', './backup.js', './runtime-config.js', './toss-client.js',
