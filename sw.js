0173f891b6c13a230cee5fec25883ad8804d63ab
dttg123 <dttg123@gmail.com>
2026-09-13 20:40:53 +0900

    Finalize v0.9 r8 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dividend-os-v0.9-r7';
+const CACHE = 'dividend-os-v0.9-r8';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './app.js', './firebase.js', './auth.js', './storage.js', './cloud.js', './backup.js', './runtime-config.js', './toss-client.js',
